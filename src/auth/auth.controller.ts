d941830bb73a711a893d827d7c80839246ba0854
mahmoud khaled <mahmoudelbannan8@gmail.com>
2024-03-07 11:58:30 +0200

    Authentication, Login/Sign Up, assign JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dtos/signUp.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
+  @Get('login')
+  login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dtos/signUp.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { name, email, password } = signUpDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = this.jwtService.sign({ email: user.email });
+    return { token };
+  }
+  async login(loginDto: LoginDto): Promise<{ token: string }> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) throw new UnauthorizedException('incorrect email or password');
+
+    const isValiedPass = bcrypt.compare(password, user.password);
+    if (!isValiedPass)
+      throw new UnauthorizedException('incorrect email or password');
+    const token = this.jwtService.sign({ email: user.email });
+    return { token };
+  }
+}
